aa38a08274dee481115e33aa3ef16c281d2c11ac
rishikeshkchapekar <chapekarrishikesh@gmail.com>
2020-09-07 23:20:49 +0530

    Stormtrooper model

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
-var rot=0;
+var rotx=0, rotz=0;
 window.addEventListener('load',function(){
 	var st = document.getElementById('ar_st');
-	window.addEventListener('click',()=>{
-		st.setAttribute("rotation", {x: 0, y: rot, z: 0});
-		rot+=10;
+	window.addEventListener('keypress',e=>{
+		switch(e.code){
+			case 38:
+				rotz-=10;
+				break;
+			case 39:
+				rotx-=10;
+			case 40:
+				rotz+=10;
+			case 37:
+				rotx+=10;	
+		}
+		st.setAttribute("rotation", {x: rotx, y: 0, z: rotz});
 	});
 });
