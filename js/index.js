c102c60b62d6615bf71e3da6fac4a05bf1bb1d31
rishikeshkchapekar <chapekarrishikesh@gmail.com>
2020-09-07 22:16:43 +0530

    Fixed rotation error

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var rot=0;
 window.addEventListener('load',function(){
 	var burger = document.getElementById('ar_burger');
 	burger.addEventListener('click',()=>{
-		this.setAttribute("rotation", `${rot} 0 0`);
+		this.setAttribute("rotation", {x: rot, y: 0, z: 0});
 		rot+=10;
 	});
 });
