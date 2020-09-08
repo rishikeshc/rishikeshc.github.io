d642c02ce3f8a18a6b71f2e440d0e68f96fc2c6f
rishikeshkchapekar <chapekarrishikesh@gmail.com>
2020-09-08 14:01:06 +0530

    Screen Touch events

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var rotx=0, rotz=0,roty=0;
-var startx=0, curx=0;
+var startx=0, curx=0,end=0;
 window.addEventListener('load',function(){
 	var st = document.getElementById('ar_burger');
 	window.addEventListener('keydown',e=>{
@@ -20,11 +20,14 @@ window.addEventListener('load',function(){
 		console.log(e.changedTouches[0]);
 		startx = parseInt(e.changedTouches[0].clientX);
 	});
+	window.addEventListener('touchend',e=>{
+		end=parseInt(e.changedTouches[0].clientX);
+	})
 	window.addEventListener('touchmove',e=>{
 		console.log("TOUCH MOVED");
 		var s = parseInt(e.changedTouches[0].clientX);
 		var mov = startx-s;
-		rotz=mov;
+		rotz=end+mov;
 
 		st.setAttribute("rotation", {x: 0, y: roty, z: rotz})
 	})
