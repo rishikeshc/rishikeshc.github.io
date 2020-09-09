4f76535c272129b19b289f4c032eb7ab1fb1e142
rishikeshkchapekar <chapekarrishikesh@gmail.com>
2020-09-09 22:48:21 +0530

    Minor updates on TM model

--- js/index.js
+++ js/index.js
@@ -5,51 +5,43 @@ var endx=0;
 function zrotation(a,b){
 	return Math.ceil(Math.sqrt(Math.pow(a,2)+Math.pow(b,2)))
 }
-AFRAME.registerComponent("move_object", {
-  init: function() {
-   this.el.addEventListener("click", (e) => {
-   	alert("Pressed");
-    // let cylinder = document.querySelector("a-cylinder")
-    // cylinder.setAttribute("visible", !cylinder.getAttribute("visible"))
-   })
-  }
-})
-// window.addEventListener('load',function(){
-// 	var st = document.getElementById('ar_tm');
-// 	window.addEventListener('keydown',e=>{
-// 		switch(e.code){
-// 			case "ArrowUp":
-// 				roty-=10;
-// 			case "ArrowRight":
-// 				rotz-=10;
-// 			case "ArrowDown":
-// 				roty+=10;
-// 			case "ArrowLeft":
-// 				rotz+=10;	
-// 		}
-// 		st.setAttribute("rotation", {x: rotx, y: roty, z: rotz});
-// 	});
 
-// 	window.addEventListener('touchstart',e=>{
-// 		console.log(e.changedTouches[0]);
-// 		startz = parseInt(e.changedTouches[0].clientX);
-// 		starty = parseInt(e.changedTouches[0].clientY);
-// 	});
-// 	window.addEventListener('touchend',e=>{
-// 		endz=parseInt(e.changedTouches[0].clientX);
-// 		endy=parseInt(e.changedTouches[0].clientY);
-// 		endx=zrotation(endz,endy);
-// 	})
-// 	window.addEventListener('touchmove',e=>{
-// 		console.log("TOUCH MOVED");
-// 		var sz = parseInt(e.changedTouches[0].clientX);
-// 		var sy = parseInt(e.changedTouches[0].clientY);
-// 		var movz = startz-sz;
-// 		var movy = starty-sy;
-// 		var movx = zrotation(movz,movy);
-// 		rotz=endz+movz;
-// 		roty=endy+movy;
-// 		rotx=endx+movx;
-// 		st.setAttribute("rotation", {x: rotx, y: roty, z: rotz})
-// 	})
-// });
+window.addEventListener('load',function(){
+	var st = document.getElementById('ar_tm');
+	window.addEventListener('keydown',e=>{
+		switch(e.code){
+			case "ArrowUp":
+				roty-=10;
+			case "ArrowRight":
+				rotz-=10;
+			case "ArrowDown":
+				roty+=10;
+			case "ArrowLeft":
+				rotz+=10;	
+		}
+		st.setAttribute("rotation", {x: rotx, y: roty, z: rotz});
+	});
+
+	window.addEventListener('touchstart',e=>{
+		console.log(e.changedTouches[0]);
+		startz = parseInt(e.changedTouches[0].clientX);
+		starty = parseInt(e.changedTouches[0].clientY);
+	});
+	window.addEventListener('touchend',e=>{
+		endz=parseInt(e.changedTouches[0].clientX);
+		endy=parseInt(e.changedTouches[0].clientY);
+		endx=zrotation(endz,endy);
+	})
+	window.addEventListener('touchmove',e=>{
+		console.log("TOUCH MOVED");
+		var sz = parseInt(e.changedTouches[0].clientX);
+		var sy = parseInt(e.changedTouches[0].clientY);
+		var movz = startz-sz;
+		var movy = starty-sy;
+		var movx = zrotation(movz,movy);
+		rotz=endz+movz;
+		roty=endy+movy;
+		rotx=endx+movx;
+		st.setAttribute("rotation", {x: rotx, y: roty, z: rotz})
+	})
+});
