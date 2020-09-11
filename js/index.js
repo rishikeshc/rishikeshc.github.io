3c32d081467bd977023dbb72910c4bb64298b0e1
rishikeshkchapekar <chapekarrishikesh@gmail.com>
2020-09-11 22:00:11 +0530

    Added swipe to change assets

--- js/index.js
+++ js/index.js
@@ -1,7 +1,4 @@
-var rotx=0, rotz=0,roty=0;
-var startz=0,endz=0;
-var starty=0,endy=0;
-var endx=0;
+var startx,endx = 0
 var curAsset="";
 function zrotation(a,b){
 	return Math.ceil(Math.sqrt(Math.pow(a,2)+Math.pow(b,2)))
@@ -23,57 +20,18 @@ function switchAsset(i){
 }
 window.addEventListener('load',function(){
 	curAsset="ar_tm";
-	AFRAME.registerComponent('right', {
-	  init: function () {
-	    this.el.addEventListener('mousedown', function (evt) {
-	    	console.log("THIS GOT CLICKED")
-	      	switchAsset(1);
-	    });
-	  }
-	});
-	AFRAME.registerComponent('left', {
-	  init: function () {
-	    this.el.addEventListener('mousedown', function (evt) {
-	      	switchAsset(-1);
-	    });
-	  }
-	});
+
 	var st = document.getElementById(curAsset);
 
-	window.addEventListener('keydown',e=>{
-		switch(e.code){
-			case "ArrowUp":
-				roty-=10;
-			case "ArrowRight":
-				rotz-=10;
-			case "ArrowDown":
-				roty+=10;
-			case "ArrowLeft":
-				rotz+=10;	
-		}
-		st.setAttribute("rotation", {x: rotx, y: roty, z: rotz});
-	});
-
 	window.addEventListener('touchstart',e=>{
-		console.log(e.changedTouches[0]);
-		startz = parseInt(e.changedTouches[0].clientX);
-		starty = parseInt(e.changedTouches[0].clientY);
+		startx = parseInt(e.changedTouches[0].clientX);
 	});
 	window.addEventListener('touchend',e=>{
-		endz=parseInt(e.changedTouches[0].clientX);
-		endy=parseInt(e.changedTouches[0].clientY);
-		endx=zrotation(endz,endy);
-	})
-	window.addEventListener('touchmove',e=>{
-		console.log("TOUCH MOVED");
-		var sz = parseInt(e.changedTouches[0].clientX);
-		var sy = parseInt(e.changedTouches[0].clientY);
-		var movz = startz-sz;
-		var movy = starty-sy;
-		var movx = zrotation(movz,movy);
-		rotz=endz+movz;
-		roty=endy+movy;
-		rotx=endx+movx;
-		st.setAttribute("rotation", {x: rotx, y: roty, z: rotz})
+		endx=parseInt(e.changedTouches[0].clientX);
+		var l = startx-endx;
+		var k=0
+		if(l< -10) k=1;
+		else if(l>10) k=-1;
+		switchAsset(k);
 	})
 });
