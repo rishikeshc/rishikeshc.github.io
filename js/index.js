b16e8a268ab5985335816f4b49e4a8d1af28a37c
rishikeshkchapekar <chapekarrishikesh@gmail.com>
2020-09-12 21:55:07 +0530

    Added some text

--- js/index.js
+++ js/index.js
@@ -1,19 +1,26 @@
 var startx,endx = 0
 var curAsset="";
+const texts = {
+	"ar_tm": "The TweetMakers Logo",
+	"ar_dish": "A white little dish",
+	"ar_burger": "Finest burger in the virtual realm",
+	"ar_st": "Worst shooters of the galaxy"
+}
 function zrotation(a,b){
 	return Math.ceil(Math.sqrt(Math.pow(a,2)+Math.pow(b,2)))
 }
 function switchAsset(i){
 	var assets = ["ar_tm","ar_dish","ar_burger","ar_st"]
 	var oldAsset = document.getElementById(curAsset);
+	var txt = document.getElementById("ar_text");
 	var index = assets.indexOf(curAsset);
-	var newIndex = 0
-	
+	var newIndex = 0	
 	if(index+i >= assets.length) newIndex = 0;
 	else if(index+i < 0) newIndex=assets.length-1;
 	else newIndex = index+i
 	var f = assets[newIndex];
 	var newAsset = document.getElementById(f);
+	txt.setAttribute("value",texts[f]);
 	oldAsset.setAttribute("visible","false");
 	newAsset.setAttribute("visible","true");
 	curAsset = f;
