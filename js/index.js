5063c4c8f35c340bcbeabfc0213d89f42cdf8cc1
rishikeshkchapekar <chapekarrishikesh@gmail.com>
2020-10-03 11:47:26 +0530

    Removed MQTT Functions

--- js/index.js
+++ js/index.js
@@ -2,15 +2,6 @@ var startx,endx = 0
 var curAsset="";
 var host="mqtt.flespi.io";
 var port = 443;
-// var mqtt;
-// var options={
-// 	timeout: 3,
-// 	userName: 'ef20jpVzcETktD5a4CKw1QUo2yatUqHXuxLBKw0NYHVh8Rg3hQp6u7lmulEezbZR',
-// 	password: '',
-// 	useSSL:true,
-// 	onSuccess: onConnect
-// }
-// mqtt.connect(options);
 var topic="AR/iot";
 
 
@@ -20,16 +11,7 @@ const texts = {
 	"ar_burger": "Finest burger in the virtual realm",
 	"ar_st": "Worst shooters of the galaxy"
 }
-// function onConnect(){
-// 	console.log("Check");
-// 	alert("Connected");
-// }
-// function sendMqtt(i){
-// 	var msg = new Paho.MQTT.Message(`${i}`)
-// 	msg.destinationName = topic;
-// 	mqtt.send(msg);
-// 	alert("Sent: ",i)
-// }
+
 function zrotation(a,b){
 	return Math.ceil(Math.sqrt(Math.pow(a,2)+Math.pow(b,2)))
 }
@@ -44,21 +26,16 @@ function switchAsset(i){
 	else newIndex = index+i
 	var f = assets[newIndex];
 	var newAsset = document.getElementById(f);
-	// txt.setAttribute("text",{value: texts[f], height: 50});
 	txt.setAttribute("value",texts[f]);
 	oldAsset.setAttribute("visible","false");
 	newAsset.setAttribute("visible","true");
 	curAsset = f;
-// 	sendMqtt(newIndex);
 }
 
 window.addEventListener('load',function(){
-// 	mqtt=new Paho.MQTT.Client(host,port,"clientjs");
-// 	mqtt.connect(options)
 	curAsset="ar_tm";
 	var txt = document.getElementById("ar_text");
 	var st = document.getElementById(curAsset);
-	// txt.setAttribute("text",{value: texts[curAsset], height: 50});
 	txt.setAttribute("value",texts[curAsset]);
 	window.addEventListener('touchstart',e=>{
 		startx = parseInt(e.changedTouches[0].clientX);
